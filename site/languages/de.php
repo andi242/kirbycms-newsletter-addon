4f303220226735ca8b32083a033044d521f65146
Andreas <andi242@gmail.com>
2015-03-18 21:38:00 +0100

    language set

---
+++ site/languages/de.php
@@ -0,0 +1,11 @@
+<?php
+
+l::set('submit', 'Abschicken');
+l::set('validemail', 'Bitte geben sie eine gültige E-Mail Adresse ein.');
+l::set('thanksubscr', 'Vielen Dank für das Interesse an unserem Newsletter.');
+l::set('headersubscr', 'Bitte tragen sie ihre E-Mail Adresse ein, um unseren Newsletter zu erhalten.');
+l::set('headerunsubscr', 'Bitte tragen sie ihre E-Mail Adresse ein, um unseren Newsletter nicht mehr zu erhalten.');
+l::set('deleteunsubscr', 'Die E-Mail Adresse wurde aus unserer Datenbank gelöscht.');
+// l::set('', '');
+// <?php echo l::get('submit') ?>
+?>
